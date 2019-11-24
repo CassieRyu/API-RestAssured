a18fd70e205d86e6092c96c867ce94aff2c57a08
CassieRyu <793399678@qq.com>
2019-11-24 19:30:28 +0800

    Add test for films

--- src/test/java/com/rest/assured/specs/FilmsTest.java
+++ src/test/java/com/rest/assured/specs/FilmsTest.java
@@ -1,45 +1,41 @@
 package com.rest.assured.specs;
 
+import com.rest.assured.Data;
 import com.rest.assured.Env;
 import com.rest.assured.Helper;
 import io.restassured.RestAssured;
 import io.restassured.response.*;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.util.List;
-
-import static io.restassured.RestAssured.given;
+import java.util.stream.Collectors;
 
 public class FilmsTest {
 
     @BeforeClass
     public void setup() {
-
         RestAssured.baseURI = Env.getBaseURL();
-//        RestAssured.baseURI = "https://ghibliapi.herokuapp.com";
         RestAssured.basePath = "/films";
     }
 
     @Test(groups = "films")
-    public void filmsContainHighScoreItems() {
-        Response response = Helper.doGetRequest("");
+    public void find_a_specific_film() {
+        String filmId = "58611129-2dbc-4a81-a72f-77ddfc1b1b49";
+        String filmTitle = "My Neighbor Totoro";
+        Response response = Helper.doGetRequest("/" + filmId);
 
-        List<String> scores = response.jsonPath().getList("rt_score");
-        System.out.println("scores: " + scores.get(0));
-      //  scores.stream().map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println("score2: " + scores.get(0));
-
+        String title = response.jsonPath().getString("title");
+        Assert.assertEquals(title, filmTitle);
     }
 
     @Test(groups = "films")
-    public void findASpecificFilm() {
+    public void films_contain_high_score_items() {
+        Response response = Helper.doGetRequest("");
 
-        String filmId = "58611129-2dbc-4a81-a72f-77ddfc1b1b49";
-        given().
-                when().
-                get("/" + filmId).
-                then().
-                assertThat()
-                .statusCode(200);
+        List<String> scores = response.jsonPath().getList("rt_score");
+        List<String> highScores = scores.stream().filter(score -> Integer.parseInt(score) >= Data.HIGH_SCORE_LIEN).collect(Collectors.toList());
+
+        Assert.assertTrue(highScores.size() > 5, "High score films not more than 5");
     }
 }
